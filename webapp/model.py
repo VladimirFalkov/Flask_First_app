09044c937d917f7c641fdd685a3fe181bf20caa0
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-08 09:48:59 +0300

    changed model

--- webapp/model.py
+++ webapp/model.py
@@ -6,9 +6,9 @@ db = SQLAlchemy()
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
+    url = db.Column(db.String, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
-    text = db.Column(db.Text, nullable=False)
+    text = db.Column(db.Text, nullable=True)
 
     def __repr__(self):
         return f'<News {self.title}  {self.url}>'
