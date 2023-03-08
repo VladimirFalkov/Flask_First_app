09ac9106fc58be4fc983bd1238afa7319746f471
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-08 09:54:26 +0300

    added unique for url in News.model

--- webapp/model.py
+++ webapp/model.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, nullable=False)
+    url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
