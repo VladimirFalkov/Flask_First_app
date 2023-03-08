fcaf83f7ac0b7a6dbda27259f76454b45ce5e6e6
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-08 06:35:06 +0300

    made def for loading news in db

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 from flask import current_app
+from webapp.model import db, News
 
 def get_page(url):
     try:
@@ -26,13 +27,11 @@ def get_python_news():
                     published = datetime.strptime(published,' %Y-%m-%d')
                 except ValueError:
                      puplished = datetime.now()
-                result_news.append(
-                    {
-                    'title': title,
-                    'url': url,
-                    'published': published,
-                    }
-                )
-            return result_news
+                save_news(title, url, published)
     return False   
-    
+
+
+def save_news(title, url, published):
+     news_news = News(title=title, url=url, published=published)
+     db.session.add(news_news)
+     db.session.commit()
