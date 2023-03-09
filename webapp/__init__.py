1004a0949065124fa71f2765babb31593856dd60
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-09 10:01:16 +0300

    made route for login_page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from webapp.model import db, News
+from webapp.forms import LoginForm
 
 def create_app():
     app = Flask(__name__)
@@ -12,5 +13,12 @@ def create_app():
         weather = {'temp_C': 11, 'FeelsLikeC':16 }
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list = news_list)
-
+    
+    @app.route('/login')
+    def login()
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+    
+    
     return app
