6b62e47699e1eb92006d17569ef645cd0f5064da
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-11 08:09:34 +0300

    added:  buttom, model for User, some details in login.html

--- webapp/model.py
+++ webapp/model.py
@@ -12,3 +12,13 @@ class News(db.Model):
 
     def __repr__(self):
         return f'<News {self.title}  {self.url}>'
+    
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self) -> str:
+        return f'User: {self.username}'
