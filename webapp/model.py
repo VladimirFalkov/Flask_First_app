0e47ebca6d38350cdf26c5cb3d067b7227814785
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-03-13 06:27:17 +0300

    added some secutity magic for passwords

--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,7 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 
 db = SQLAlchemy()
@@ -14,11 +17,17 @@ class News(db.Model):
         return f'<News {self.title}  {self.url}>'
     
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
     role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password)
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self) -> str:
         return f'User: {self.username}'
